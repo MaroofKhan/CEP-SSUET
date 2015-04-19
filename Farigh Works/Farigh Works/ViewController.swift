0db6c67ef45f04e474faddd023ef8ea20859337b
MaroofKhan <panaswiftteachers@Zias-MacBook-Pro.local>
2015-04-19 18:15:22 +0500

    April 19 - 2015

---
+++ Farigh Works/Farigh Works/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Farigh Works
+//
+//  Created by Panaswift Teachers on 19/04/2015.
+//  Copyright (c) 2015 Panaswift Teachers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    @IBAction func addItem(sender: UIButton) {
+        println("Add item pressed!")
+    
+    }
+
+}
+
